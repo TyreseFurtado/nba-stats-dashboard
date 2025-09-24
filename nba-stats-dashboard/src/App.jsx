30ee66aca8414dbd280d6cf2f01eebc8b6793681
Tyrese Furtado <tf00534@surrey.ac.uk>
2025-09-24 20:01:24 +0100

    Couldn't implement a leaderboard on the stats page due to API limitations. May come back later to improve styling however functionality works as expected.

--- nba-stats-dashboard/src/App.jsx
+++ nba-stats-dashboard/src/App.jsx
@@ -6,11 +6,11 @@ import './App.css'
 import axios from 'axios'
 import { FaStar } from 'react-icons/fa'
 import PlayerCard from './playerCard.jsx'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import TeamsDropdown from './TeamsDropdown.jsx'
 import Header from './Header.jsx'
 
-function App({ favouritePlayers, onFavouriteToggle}) {
+function App({ favouritePlayers, onFavouriteToggle }) {
   const apiKey = import.meta.env.VITE_API_KEY;
 
   const api = axios.create({
@@ -54,28 +54,28 @@ function App({ favouritePlayers, onFavouriteToggle}) {
   const isFavourited = selectedPlayer ? favouritePlayers.some(
     fav => fav.id === selectedPlayer.id) : false;
 
-  
+
   const filteredPlayers = players.filter(player => {
     let passesPosition = true;
     let passesTeam = true;
-    
-    if (positionFilter){
+
+    if (positionFilter) {
       const playerPosition = player.position || '';
-      if(playerPosition.includes('-')){
+      if (playerPosition.includes('-')) {
         const positions = playerPosition.split('-');
         passesPosition = positions.includes(positionFilter);
       } else {
         passesPosition = player.position === positionFilter;
       }
     }
-    
-    if(selectedTeam) {
+
+    if (selectedTeam) {
       passesTeam = player.team.id.toString() === selectedTeam;
     }
 
     return passesPosition && passesTeam;
   });
-  
+
 
 
 
@@ -103,7 +103,7 @@ function App({ favouritePlayers, onFavouriteToggle}) {
     if (!searchTerm.trim()) {
       setError('Please enter a player name');
       setLoading(false);
-      return; 
+      return;
     }
     if (searchTerm.length < 2) {
       setError('Please enter at least 2 characters;');
@@ -121,73 +121,76 @@ function App({ favouritePlayers, onFavouriteToggle}) {
           per_page: 10,
         }
       });
-      
+
       if (response.data.data && response.data.data.length > 0) {
         setPlayers(response.data.data);
       } else {
-        setError("No players found for "+ searchTerm);
+        setError("No players found for " + searchTerm);
       }
     }
-    
-   catch (err) {
-    console.error('Search error:', err);
-    setError('An error occurred while searching. Please try again.');
-  } finally {
-    setLoading(false);
-  }
+
+    catch (err) {
+      console.error('Search error:', err);
+      setError('An error occurred while searching. Please try again.');
+    } finally {
+      setLoading(false);
+    }
   }
 
   return (
     <div>
-      <Header/>
-      <div className ="Search-container">
-      <input type="text" placeholder="Search players..." value={searchTerm} className ="search-bar" 
-        onChange={handleInputChange}
-        onKeyDown={(e) => {
-          if(e.key === 'Enter' && !loading) {
-            searchPlayers();
-          }
-        }} />
-      <button className="search-button" onClick={searchPlayers} disabled={loading}>{loading ? 'Searching...' : 'Search'}</button>
-      <div className="filter-container">
-        <select value={positionFilter} onChange={handlePositionFilterChange}>
-          {["Any Position","G","F","C"].map(pos => (
-            <option key={pos} value={pos === "Any Position" ? "" : pos}>
-              {pos}
-            </option>
-          ))}
-        </select>
+      <Header />
+      <div className="Search-container">
+        <div className="search-input-group">
+          <input type="text" placeholder="Search players..." value={searchTerm} className="search-bar"
+            onChange={handleInputChange}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter' && !loading) {
+                searchPlayers();
+              }
+            }} />
+          <button className="search-button" onClick={searchPlayers} disabled={loading}>{loading ? 'Searching...' : 'Search'}</button>
+        </div>
+
+        <div className="filter-container">
+          <select value={positionFilter} onChange={handlePositionFilterChange}>
+            {["Any Position", "G", "F", "C"].map(pos => (
+              <option key={pos} value={pos === "Any Position" ? "" : pos}>
+                {pos}
+              </option>
+            ))}
+          </select>
+        </div>
+
+        <div className="team-filter">
+          <TeamsDropdown onTeamSelect={setSelectedTeam} />
+        </div>
+      </div>
+      <div className="players-container">
+        {filteredPlayers.map(player => (
+          <PlayerCard
+            key={player.id}
+            player={player}
+            onClick={handlePlayerClick}
+
+          />
+        ))}
+        {error && <p className="error-message">{error}</p>}
+        {loading && <p>Loading...</p>}
+        {players.length === 0 && !loading && !error && <p>No players to display. Please search for a player.</p>}
+
+
       </div>
 
-      <div className="team-filter">
-        <TeamsDropdown onTeamSelect={setSelectedTeam}/>
-      </div>
-      </div>
-      <div className ="players-container">
-      {filteredPlayers.map(player =>  (
-        <PlayerCard
-        key={player.id}
-        player={player}
-        onClick={handlePlayerClick}
-
-        />
-      ))}
-      {error && <p className="error-message">{error}</p>}
-      {loading && <p>Loading...</p>}
-      {players.length === 0 && !loading && !error && <p>No players to display. Please search for a player.</p>}
-
-      
-      </div>
-
-      <div className ={`sidebar ${sidebarOpen ? 'open' : ''}`}>
+      <div className={`sidebar ${sidebarOpen ? 'open' : ''}`}>
         <div className="sidebar-content">
           <button className="close-button" onClick={() => setSidebarOpen(false)}>Close</button>
 
           {selectedPlayer && (
-            <div className ="player-details">
+            <div className="player-details">
               <h2>{selectedPlayer.first_name} {selectedPlayer.last_name}</h2>
-              <FaStar className={`favourite-symbol ${isFavourited ? 'favourited' : ''}`}  onClick={() => handleFavouriteClick(selectedPlayer)}/>
-              
+              <FaStar className={`favourite-symbol ${isFavourited ? 'favourited' : ''}`} onClick={() => handleFavouriteClick(selectedPlayer)} />
+
               <div className="detail-section">
                 <h3>Basic Information</h3>
                 <div className="detail-grid">
@@ -255,15 +258,15 @@ function App({ favouritePlayers, onFavouriteToggle}) {
               </div>
             </div>
           )}
-            </div>
+        </div>
 
-          
-          
+
+
       </div>
 
       {sidebarOpen && <div className="sidebar-overlay" onClick={() => setSidebarOpen(false)}></div>}
     </div>
-   
+
   )
 }
 
