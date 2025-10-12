6620c4a0c1bc8e140c4b75e560fac40284a81450
tf00534 <tf00534@surrey.ac.uk>
2025-10-12 14:56:37 +0100

    Renamed the project

--- nba-player-directory/src/Header.jsx
+++ nba-player-directory/src/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
 
     return (
         <header className="dashboard-header">
-            <h1 className="dashboard-title">NBA Stats Dashboard</h1>
+            <h1 className="dashboard-title">NBA Player Directory</h1>
             <div className="header-actions">
                 <Link to="/" className="header-link">Search</Link>
                 <Link to="/favourites" className="header-link">Favourites</Link>
